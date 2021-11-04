8f5b776cb37aff3724d2211ea4f924027a2fa412
jweyer <jweyer@scottkon-lptp.redmond.corp.microsoft.com>
2021-11-04 12:24:22 -0700

    testing

--- snaggerRuby.rb
+++ snaggerRuby.rb
@@ -3,6 +3,8 @@
 #hello
 puts "hello world"
 a = [1, 2, 3]
-a.map {|x| x**2}
-shlgas
-puts "This is the array squared #{a}"
+b = a.map {|x| x**2}
+puts "This is the array squared #{b}"
+c = a.inject {|sum, x| sum + x}
+puts "This is the value of the array squared and summed #{c}"
+puts "saving "
