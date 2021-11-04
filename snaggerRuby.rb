1f95b5fee5da97d767c7c3bb77060bb210f1137e
jweyer8 <jweyermtb8@gmail.com>
2021-11-04 14:00:17 -0700

    added another function

--- snaggerRuby.rb
+++ snaggerRuby.rb
@@ -11,9 +11,21 @@ puts "This is the value of the array squared and summed #{c}"
 
 ## do the same as above with a funcion 
 def square(*args)
-    puts "here is the array of arguments #{x}"
+    puts "here is the array of arguments #{args}"
     args.map{|x| x**2}
+end
+
+## do the the same as above with another funtion have user input args
+def sum(args)
+    puts "this is the array of args you entered: #{args}"
+    args.inject{|sum, x| sum + x}
 end
 
 a = square(1, 2, 3)
 puts "This is the array of args squared #{a}"
+
+puts "enter arguments to sum \n enter q to quit"
+a = []
+(0...3).each{|x| a << gets.chomp.to_i}
+b = sum(a)
+puts "this is the is the sum of your arguments: #{b}"
